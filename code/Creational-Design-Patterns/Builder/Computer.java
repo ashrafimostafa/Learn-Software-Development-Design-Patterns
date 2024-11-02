5aa4e774af7f6c9633ad84a652f83aad2c8b3d0d
Mostafa <ashrafimostafa@gmail.com>
2024-11-02 23:35:49 +0330

    Add creational design patterns slides and code

---
+++ code/Creational-Design-Patterns/Builder/Computer.java
@@ -0,0 +1,8 @@
+public class Computer {
+    private String processor;
+    private int memory;
+    private int storage;
+    private String graphicsCard;
+
+    // Constructor and getters
+}
---
+++ code/Creational-Design-Patterns/Builder/ComputerBuilder.java
@@ -0,0 +1,7 @@
+public interface ComputerBuilder {
+    ComputerBuilder buildProcessor(String processor);
+    ComputerBuilder buildMemory(int memory);
+    ComputerBuilder buildStorage(int storage);
+    ComputerBuilder buildGraphicsCard(String graphicsCard);
+    Computer build();
+}
